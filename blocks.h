9e5c10143af19698a00ed9250f27325384ed05c9
zoomiti <sotoluis90@gmail.com>
2021-08-26 22:01:12 -0700

    Added pacupdates module

--- blocks.h
+++ blocks.h
@@ -4,10 +4,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ "", PATH("sb-cpu"),    5, 4 },
-	{ "", PATH("sb-memory"), 5, 3 },
-	{ "", PATH("sb-volume"), 0, 2 },
-	{ "", PATH("sb-clock"),  1, 1 },
+	{ "", PATH("sb-cpu"),           5, 4 },
+	{ "", PATH("sb-memory"),        5, 3 },
+	{ "", PATH("sb-pacpackages"), 360, 5 },
+	{ "", PATH("sb-volume"),        0, 2 },
+	{ "", PATH("sb-clock"),         1, 1 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
